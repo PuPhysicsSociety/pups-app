685af8d307071aad21b1b212cdc7707345e5dba4
NOOB-3301 <arka11362@gmail.com>
2026-06-12 00:33:07 +0530

    Google search console integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,10 @@ import { Analytics } from '@vercel/analytics/next';
 
 export const metadata = {
   title: 'PUPS — Presidency University Physics Society',
-  description: 'The Presidency University Physics Society organises weekly colloquia, panel discussions, and thematic events that cultivate a vibrant academic culture centred around the exploration of physics.'
+  description: 'The Presidency University Physics Society organises weekly colloquia, panel discussions, and thematic events that cultivate a vibrant academic culture centred around the exploration of physics.',
+  verification: {
+    google: 'E0T6vNd36l_CG2EVyVIU4UC3jQEgkjUZM5A1fkN_6PQ',
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {

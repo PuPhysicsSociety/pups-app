dc305e5b7727029c9a422dfc53f09cd1b8328f8d
Aritra Bakshi <aritra1310@gmail.com>
2026-06-11 19:43:07 +0530

    Update EventCard.tsx

--- components/ui/EventCard.tsx
+++ components/ui/EventCard.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
 import Link from 'next/link';
-import { Event } from '../../types';
+import type { Event } from '../../types';
 import { getImageUrl } from '../../lib/api';
 
 interface EventCardProps {
